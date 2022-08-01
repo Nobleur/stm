fa6aa4d008d2cdbbe3be51b03b30be1762d6e66b
Nobleur <lucassousa.silva42@gmail.com>
2022-08-01 20:54:14 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+// Execute script after page load
+
+window.onload = function digital_fn(){
+
+    //Toggle Button 
+
+    let toggle = document.querySelector("#nav .toggle-btn")
+    let collapse = document.querySelector("#nav .collapse")
+
+    toggle.addEventListener('click', function(event){
+        collapse.classList.toggle('active')
+       // console.log(toggle)
+    });
+
+    //Masonry JS
+
+    let grid = document.querySelector("#site-main .recent-work-area .images-flex")
+
+    let msnry = new Masonry(grid, {
+        itemSelector: '.flex-item',
+        gutter: 100,
+        fitWidth: true
+    })
+
+}
+
+// Rellax JS Code
+
+var rellax = new Rellax('.rellax',{
+    center: true
+})
